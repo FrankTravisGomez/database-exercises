54b1e8d403681246f55d9b031e045a289cddb487
Travis Gomez <frank.travis.gomez@gmail.com>
2023-04-19 10:07:50 -0500

    create complete wrking on read

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+INSERT INTO albums_migration.sql(artist, name, release_date, sales, genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock, heavy metal, folk rock'),
+       ('Michael Jackson', 'Bad', 1987, 21.1, 'Pop, rhythm and blues, funk and rock'),
+       ('Metallica', 'Metallica', 1991, 22.7, 'Heavy Metal'),
+       ('The Beatles', 'Abbey Road', 1996, 16.9, 'Rock'),
+       ('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive rock');
