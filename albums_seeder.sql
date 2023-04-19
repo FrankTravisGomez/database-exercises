444b608d5c86a02d1afb2ed4a4861ac68d9f901d
Travis Gomez <frank.travis.gomez@gmail.com>
2023-04-19 13:59:12 -0500

    complete first two of exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,7 +1,10 @@
 USE codeup_test_db;
-INSERT INTO albums_migration.sql(artist, name, release_date, sales, genre)
+INSERT INTO albums(artist, name, release_date, sales, genre)
 VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock, heavy metal, folk rock'),
        ('Michael Jackson', 'Bad', 1987, 21.1, 'Pop, rhythm and blues, funk and rock'),
        ('Metallica', 'Metallica', 1991, 22.7, 'Heavy Metal'),
        ('The Beatles', 'Abbey Road', 1996, 16.9, 'Rock'),
-       ('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive rock');
+       ('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive rock'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.3, 'Progressive rock'),
+       ('Nirvana', 'Nevermind', 1991, 18.9, 'Grunge, alternative rock');
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,7 @@
 USE codeup_test_db;
-SELECT * FROM albums_migration.sql;
+SELECT name AS 'The name of all albums by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' FROM albums WHERE release_date = 1967;
+SELECT genre AS 'The genre for Nevermind' FROM albums WHERE genre = 'Grunge, alternative rock';
+SELECT name AS 'Which albums were released in the 1990s' FROM albums WHERE release_date >= 1990;
+SELECT name AS 'Which albums had less than 20 million certified sales' FROM albums WHERE sales >= 20;
+SELECT name AS 'All the albums with a genre of "Rock"' FROM albums WHERE genre = 'Rock';
