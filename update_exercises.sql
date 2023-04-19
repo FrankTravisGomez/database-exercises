407fd0ee143888446fb289903c021d1c77e01a2a
Travis Gomez <frank.travis.gomez@gmail.com>
2023-04-19 14:44:03 -0500

    update has been complete

---
+++ update_exercises.sql
@@ -0,0 +1,16 @@
+# USE codeup_test_db;
+SELECT * FROM albums;
+UPDATE albums
+SET sales = sales* 10;
+SELECT * FROM albums;
+
+SELECT * FROM albums;
+SELECT * FROM albums;
+UPDATE albums
+SET release_date = 1800
+WHERE release_date <= 1980;
+
+SELECT * FROM albums;
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
